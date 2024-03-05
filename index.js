8b05e0168b8833eaab0fcabe5a4e442c8d38f82c
Shashwat Pathak <shashwatpathak18@gmail.com>
2024-03-05 11:35:18 +0530

    added exercise 3.4

--- index.js
+++ index.js
@@ -50,6 +50,20 @@ app.get("/info", (request, response) => {
   response.send(`<p>Phonebook has info for ${phoneBook.length} people </p> <p>
       ${new Date().toString()}`);
 });
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const toDelete = phoneBook.find((person) => person.id === id);
+
+  if (toDelete) {
+    phoneBook = phoneBook.filter((per) => per.id !== id);
+    response.json({ sucess: `${id} deleted` });
+  }
+  else{
+    response.json({error: "Person not found"})
+  }
+});
+
 const PORT = 3023;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
